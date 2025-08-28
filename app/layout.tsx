c6fa2012db9f27291e3a2e9e46a8f7bfff918661
fed999 <p1voxn1@gmail.com>
2025-08-28 13:57:07 +0900

    initial

---
+++ app/layout.tsx
@@ -0,0 +1,20 @@
+// app/layout.tsx
+import './globals.css'
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Skillbox Clone',
+  description: 'Практический курс по созданию презентаций',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="ru">
+      <body>{children}</body>
+    </html>
+  )
+}
---
+++ pages/_app.tsx
@@ -0,0 +1,18 @@
+// pages/_app.tsx
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import { useEffect } from "react";
+
+
+function MyApp({ Component, pageProps }: AppProps) {
+    useEffect(() => {
+        const script = document.createElement("script");
+        document.body.appendChild(script);
+      }, []);
+  return <Component {...pageProps} />
+}
+
+export default MyApp
